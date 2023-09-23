889d959888d62402082167ecdb5ed99be291c392
stampaaaron <aaron@stampa.ch>
2023-09-23 16:10:09 +0000

    feat: add pwa icon

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,18 @@ export default defineConfig({
         theme_color: "#aaaaaa",
         background_color: "#aaaaaa",
         display: "standalone",
-        icons: [],
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
